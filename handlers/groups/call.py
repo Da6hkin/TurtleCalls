fcdd7267db56d1b729e98af0e466745136490e39
Dmytro <dashkin.dima@rl.odessa.ua>
2024-01-04 21:16:56 +0200

    Added Degen and Alpha calls + mcap

--- handlers/groups/call.py
+++ handlers/groups/call.py
@@ -27,17 +27,18 @@ async def echo(message: types.Message, state: FSMContext):
                 "X-BLOBR-KEY": f"{data.config.DEXTOOLS}"
             }
             async with aiohttp.ClientSession(headers=headers) as session:
-                async with session.get(f"https://open-api.dextools.io/free/v2/token/solana/{splitted_args[0]}") as resp:
+                async with session.get(
+                        f"https://open-api.dextools.io/free/v2/token/solana/{splitted_args[0]}/info") as resp:
                     json_body = await resp.json()
                     if json_body["statusCode"] != 200:
                         await message.reply(
                             "Please re-check the CA that you've sent\nHaving problems finding this CA on solana blockchain")
                     else:
                         reply_message = await message.reply(
-                            f"You sure you want to make a call on ${json_body['data']['symbol']}",
+                            f"You sure you want to make a call on ${json_body['data']['symbol']}\n💰 FDV: <code>{humanize_number(json_body['fdv'])}</code>",
                             reply_markup=yes_or_no)
                         state_data[reply_message.message_id] = [message.from_user.id, json_body['data']['symbol'],
-                                                                splitted_args[0]]
+                                                                splitted_args[0], json_body['fdv']]
                         await state.update_data(state_data)
 
 
@@ -50,12 +51,13 @@ async def echo(call: types.CallbackQuery, state: FSMContext):
         if state_data[message_id][0] == call.from_user.id:
             coin_ticker = state_data[message_id][1]
             contract = state_data[message_id][2]
-            if call.data == "yes":
+            mcap = humanize_number(state_data[message_id][3])
+            if call.data != "no":
                 users = await db.select_all_users()
                 coros = []
                 for user in users:
                     coros.append(bot.send_message(chat_id=user['telegram_id'],
-                                               text=f"New Solana call from {call.from_user.full_name}\n<b>${coin_ticker}</b>\n<code>{contract}</code>\n<a href='https://t.me/mcqueen_bonkbot?start=ref_o895c_ca_{contract}'>BONK BUY</a> | <a href='https://birdeye.so/token/{contract}?chain=solana'>Birdeye</a>"))
+                                                  text=f"New Solana <b>{call.data}</b> call from <b>{call.from_user.first_name}</b>\n<b>${coin_ticker}</b>\n💰 FDV: <code>{mcap}</code>\n<code>{contract}</code>\n<a href='https://t.me/mcqueen_bonkbot?start=ref_o895c_ca_{contract}'>BONK BUY</a> | <a href='https://birdeye.so/token/{contract}?chain=solana'>Birdeye</a>"))
 
                 await asyncio.gather(*coros, return_exceptions=True)
                 await call.message.edit_reply_markup(reply_markup=None)
@@ -67,3 +69,23 @@ async def echo(call: types.CallbackQuery, state: FSMContext):
                 await call.message.reply("Call cancelled")
                 del state_data[message_id]
                 await state.update_data(state_data)
+
+
+def humanize_number(value, fraction_point=1):
+    powers = [10 ** x for x in (12, 9, 6, 3, 0)]
+    human_powers = ('T', 'B', 'M', 'K', '')
+    is_negative = False
+    if not isinstance(value, float):
+        value = float(value)
+    if value < 0:
+        is_negative = True
+        value = abs(value)
+    for i, p in enumerate(powers):
+        if value >= p:
+            return_value = str(round(value / (p / (10.0 ** fraction_point))) /
+                               (10 ** fraction_point)) + human_powers[i]
+            break
+    if is_negative:
+        return_value = "-" + return_value
+
+    return return_value
--- keyboards/inline/call_keyboards.py
+++ keyboards/inline/call_keyboards.py
@@ -1,11 +1,14 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-yes_or_no = InlineKeyboardMarkup(inline_keyboard=[
+yes_or_no = InlineKeyboardMarkup(row_width=2,inline_keyboard=[
 
     [
-        InlineKeyboardButton(text="Yes ✅", callback_data="yes"),
+        InlineKeyboardButton(text="Degen Call 🎲", callback_data="Degen"),
     ],
-[
+    [
+        InlineKeyboardButton(text="Alpha Call 👑", callback_data="Alpha"),
+    ],
+    [
         InlineKeyboardButton(text="No ⛔️", callback_data="no"),
     ]
 ])
